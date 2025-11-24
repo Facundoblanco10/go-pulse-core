a6236063467507e095ca143b910a6dc40e9fce4f
Facundo <facundoblancov@gmail.com>
2025-11-24 20:49:36 -0300

    Health check endpoint basic with gin framework

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,1 +1,15 @@
-package server
+package main
+
+import (
+	"log"
+
+	"github.com/Facundoblanco10/go-pulse-core/internal/api"
+)
+
+func main() {
+	r := api.NewRouter()
+
+	if err := r.Run(":8080"); err != nil {
+		log.Fatal(err)
+	}
+}
